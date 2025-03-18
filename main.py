a67d6fe1bed2791bf40a704d9c2a261dc390f171
Gabe-Manaog <alonzogabriel.manaog@ontariotechu.net>
2025-03-18 11:30:01 -0400

    Added Accounts and Main

---
+++ main.py
@@ -0,0 +1,5 @@
+from read import read_old_bank_accounts
+from write import write_new_current_accounts
+from print_error import log_constraint_error
+
+accounts=read_old_bank_accounts('master_accounts.txt')
